193d31004b9fb343c992573fc964553a31c938d6
Efrain Cataño <efrain.catano97@gmail.com>
2023-03-11 18:11:25 -0300

    fixing put and delete endpoints

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,6 @@ router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getOneUser);
 router.post('/', userController.createUser);
 router.put('/:id', userController.updateUser);
-router.delete('/', userController.deleteUser);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
